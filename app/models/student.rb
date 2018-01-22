4a2f6af9d47f5a402b9c1a7644fb6bab02e12f47
Mariano Moretti <marianomoretti.87@gmail.com>
2018-01-22 19:49:12 -0300

    Cascade delete dependents for students and tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :signature_year
+  has_many :test_notes, dependent: :destroy
 
   validates :dni, :email, :number, :name, :last_name, presence: true
   validates_with EmailValidator
