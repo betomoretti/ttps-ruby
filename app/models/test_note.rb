592401271e22df372ba55a68a97a50f038b7fa04
Mariano Moretti <marianomoretti.87@gmail.com>
2018-02-06 21:54:44 -0300

    Presence validation added to note for test notes

--- app/models/test_note.rb
+++ app/models/test_note.rb
@@ -1,6 +1,7 @@
 class TestNote < ApplicationRecord
   belongs_to :student
   belongs_to :test
+  validates :note, :test, :student, presence: true
 
   def approved?
     return 'ausent' unless note.present?
