02f93cc9d2c1c554482c0405283f3a2351172ffd
Mariano Moretti <marianomoretti.87@gmail.com>
2018-02-06 21:38:01 -0300

    I18n added for student creation errors

--- app/models/concerns/email_validator.rb
+++ app/models/concerns/email_validator.rb
@@ -1,7 +1,7 @@
 class EmailValidator < ActiveModel::Validator
   def validate(record)
     unless record.email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[:email] << "not valid"
+      record.errors.add(:email, :invalid)
     end
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,5 @@ class Student < ApplicationRecord
 
   validates :dni, :email, :number, :name, :last_name, presence: true
   validates_with EmailValidator
-  validates :dni, :number, format: { with: /\d/,
-                                    message: "only allows digits" }
+  validates :dni, :number, format: { with: /\d/, message: :format }
 end
